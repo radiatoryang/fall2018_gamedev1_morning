effa90a2441920929dda09da90465ff92c2048cd
Robert Yang <yang.robert.w@gmail.com>
2018-09-25 17:15:05 -0400

    week03, intro to physics (physics demo scene + trigger demo scene)

---
+++ week03_introToPhysics/Assets/scripts/TriggerTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// put this script on a trigger
+public class TriggerTest : MonoBehaviour {
+
+	// for OnTriggerEnter to work, at least one of the objects
+	// must have a Rigidbody
+	void OnTriggerEnter(Collider activator)
+	{
+		// when something enters this trigger, destroy / delete it
+		// Destroy( activator ); // will only delete the Collider
+		Destroy( activator.gameObject ); // will destroy whole thing
+		// Destroy( this.gameObject ); // destroy this trigger?
+		
+	}
+	
+	
+}
